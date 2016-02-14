47742df177e17f32dfcf487d65c9b6b020ac6a24
Tom Schaible <tschaible@gmail.com>
2016-02-13 20:55:02 -0500

    Added in a basic forecast wrapper for openweathermaps.com api

--- modules/openweathermap.js
+++ modules/openweathermap.js
@@ -4,7 +4,7 @@ var request = require('request');
 var qs = require('querystring');
 var debug = require('debug')('slack-weather:openweathermap');
 
-var apiBase = 'http://api.openweathermap.org/data/2.5/weather'
+var apiBase = 'http://api.openweathermap.org/data/2.5/'
 
 
 function Weather(apiKey) {
@@ -20,35 +20,87 @@ function Weather(apiKey) {
 	this.apiKey = apiKey || process.env.OPENWEATHERMAP_API_KEY;
 
 	this.getWeatherByZip = function(zip, callback) {
-		request.get(apiBase + '?' + qs.stringify({
+		request.get(apiBase + 'weather?' + qs.stringify({
 				zip: zip + ',us',
 				appid: this.apiKey
 			}),
 			function handleRequest(err, res, body) {
-				if ( !err && res.statusCode == 200 ) {
+				if (!err && res.statusCode == 200) {
 					var owmResponse = JSON.parse(body);
-					if ( !owmResponse.main || !owmResponse.main.temp ) {
+					if (!owmResponse.main || !owmResponse.main.temp) {
 						var error = new Error('Unknown response from weather service');
 						error.status = 500;
-						debug('Unknown Weather Service response: \n%s',body);
+						debug('Unknown Weather Service response: \n%s', body);
 						return callback(error, null);
 					}
 					var response = {};
 					response.city = owmResponse.name;
 					response.tempC = owmResponse.main.temp - 273.15;
 					response.tempF = (response.tempC * 1.8) + 32;
-					response.description = owmResponse.weather.map(function(obj){return obj.description});
-					return callback(null,response);
-				} else if ( !err ) {
+					response.description = owmResponse.weather.map(function(obj) {
+						return obj.description
+					});
+					return callback(null, response);
+				} else if (!err) {
 					var error = new Error('Error from weather service');
 					error.status = 500;
-					debug('Weather Service error: \n%s',body);
+					debug('Weather Service error: \n%s', body);
 					return callback(error, null);
 				} else {
 					return callback(err, null);
 				}
 			});
 	};
+
+	this.getForecastByZip = function(zip, callback) {
+		var key = this.apiKey;
+		//use zip call to get city name
+		this.getWeatherByZip(zip, function(err, zipResponse) {
+			if (err) {
+				return callback(err, null);
+			} else {
+				request.get(apiBase + 'forecast/daily?' + qs.stringify({
+						cnt: 5,
+						q: zipResponse.city + ',us',
+						appid: key
+					}),
+					function handleRequest(err, res, body) {
+						if (!err && res.statusCode == 200) {
+							var owmResponse = JSON.parse(body);
+							if (!owmResponse.list ) {
+								var error = new Error('Unknown response from weather service');
+								error.status = 500;
+								debug('Unknown Forecast Service response: \n%s', body);
+								return callback(error, null);
+							}
+							var response = {};
+							response.city = zipResponse.city;
+							response.forecast = owmResponse.list.map(function(obj) {
+								var item  = {
+									timestamp: obj.dt,
+									lowTempC: obj.temp.min - 273.15,
+									highTempC: obj.temp.max - 273.15,
+									description: obj.weather.map(function(w) {
+										return w.description;
+									})
+								};
+								item.lowTempF =(item.lowTempC * 1.8) + 32;
+								item.highTempF =(item.highTempC * 1.8) + 32;
+								return item;
+							});
+							return callback(null, response);
+						} else if (!err) {
+							var error = new Error('Error from weather service');
+							error.status = 500;
+							debug('Weather Service error: \n%s', body);
+							return callback(error, null);
+						} else {
+							return callback(err, null);
+						}
+					});
+			}
+		});
+	}
 }
 
 module.exports = Weather;
--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,18 @@
 var Weather = require('../modules/openweathermap');
 var express = require('express');
 var util = require('util');
+var moment = require('moment');
 var router = express.Router();
 
 
 //new Weather API using the key defaulted from the environment
 var weather = new Weather();
 
-/* GET home page. */
+/* current weather */
 router.post('/weather', function(req, res, next) {
-	var zip = req.body.text;
-	var fahrenheit = !req.body.command.includes('c');
+	var tokens = req.body.text.split(' ');
+	var zip = tokens[0];
+	var fahrenheit = tokens.length <= 1 || tokens[1].trim().toLowerCase() === 'f'  ;
 	if (!zip || !isValidUSZip(zip.trim())) {
 		res.json({
 			text: "I'm sorry I didn't understand.  Please use a US zip"
@@ -29,6 +31,42 @@ router.post('/weather', function(req, res, next) {
 					fahrenheit ? result.tempF.toFixed(1) : result.tempC.toFixed(1),
 					fahrenheit ? 'F' : 'C',
 					result.description.join(', '))
+			});
+		}
+	});
+
+});
+
+router.post('/forecast', function(req, res, next) {
+	var tokens = req.body.text.split(' ');
+	var zip = tokens[0];
+	var fahrenheit = tokens.length <= 1 || tokens[1].trim().toLowerCase() === 'f'  ;
+	if (!zip || !isValidUSZip(zip.trim())) {
+		res.json({
+			text: "I'm sorry I didn't understand.  Please use a US zip"
+		});
+	}
+	weather.getForecastByZip(zip.trim(), function handleResponse(err, result) {
+		if (err) {
+			res.json({
+				text: "I'm sorry I couldn't process your request.  Please try again later"
+			});
+		} else {
+			var forecast = result.forecast.map(function(f){
+				return util.format('%s High %s°%s Low %s°%s %s',
+					moment.unix(f.timestamp).format('M/D'),
+					fahrenheit ? f.highTempF.toFixed(1) : f.highTempC.toFixed(1),
+					fahrenheit ? 'F' : 'C',
+					fahrenheit ? f.lowTempF.toFixed(1) : f.lowTempC.toFixed(1),
+					fahrenheit ? 'F' : 'C',
+					f.description.join(', ')
+				);
+			}).join('\n');
+			res.json({
+				response_type: "in_channel",
+				text: util.format('Here\'s the upcoming weather in %s\n%s',
+					result.city,
+					forecast)
 			});
 		}
 	});
