25960276c265a483e3b4102a79787c0864727526
Tom Schaible <tschaible@gmail.com>
2016-02-21 20:35:48 -0500

    default docker host for integration tes to 127.0.0.1 #5

--- scripts/pre-integration-test.sh
+++ scripts/pre-integration-test.sh
@@ -5,6 +5,10 @@ if [ $(docker ps -a -q --filter 'label=environment=slack_weather_integration_tes
 	docker stop $(docker ps -a -q --filter 'label=environment=slack_weather_integration_test')
 fi
 
+if [ -z "$DOCKER_HOST_ADDRESS" ]; then
+	DOCKER_HOST_ADDRESS="127.0.0.1"
+fi
+
 #build docker container for integration test and run it
 docker build --force-rm -t tschaible/slack-weather:integration-test . && \
 docker run -e OPENWEATHERMAP_API_KEY=$OPENWEATHERMAP_API_KEY -d \
