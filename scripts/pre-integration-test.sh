9d3a8973c19b4503ab04442b4dcb1d21c67e486f
Tom Schaible <tschaible@gmail.com>
2016-02-21 20:10:48 -0500

    additional cleanup on pre/post integration test scripts #5

--- scripts/pre-integration-test.sh
+++ scripts/pre-integration-test.sh
@@ -1,13 +1,19 @@
 #!/bin/bash 
+
+#stop old containers if they ar running
+if [[ $(docker ps -a -q --filter 'label=environment=slack_weather_integration_test') ]]; then
+	docker stop $(docker ps -a -q --filter 'label=environment=slack_weather_integration_test')
+fi
+
+#build docker container for integration test and run it
 docker build --force-rm -t tschaible/slack-weather:integration-test . && \
 docker run -e OPENWEATHERMAP_API_KEY=$OPENWEATHERMAP_API_KEY -d \
 	-p 6789:3000 --label environment=slack_weather_integration_test tschaible/slack-weather:integration-test 
 
 #wait for server to come up, up to 20s
-RETRY=0;
-until ((RETRY>3)) || $(curl --output /dev/null --silent --head http://$DOCKER_HOST_ADDRESS:6789); 
-do 
-	RETRY=$(($RETRY + 1));
-	sleep 5; 
-	printf '.';
+RETRY=0
+until [[ $RETRY -gt 3 ]] || $(curl --output /dev/null --silent --head http://$DOCKER_HOST_ADDRESS:6789); do 
+	RETRY=$(($RETRY + 1))
+	sleep 5
+	printf '.'
 done;
